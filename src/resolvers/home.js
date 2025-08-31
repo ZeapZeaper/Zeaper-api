e77f88a4945722aa0205d1be151a7f0ed90efd2a
foster <foster.ogwudu@yahoo.com>
2025-08-31 23:00:55 +0100

    modify home page

--- src/resolvers/home.js
+++ src/resolvers/home.js
@@ -1,7 +1,11 @@
 const path = require("path");
+const { ENV } = require("../config");
+
 const home = (req, res) => {
-  return res.sendFile(path.join(`${__dirname}/../views/index.html`));
+  return res.sendFile(path.join(`${__dirname}/../views/index.html`), {
+    headers: { "Content-Type": "text/html", "X-Env": `${ENV}` },
+  });
 };
 module.exports = {
-  getHome: home
+  getHome: home,
 };
